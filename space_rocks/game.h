16bc1ef6bcac074b508617d6c14c4e0fb39265b0
Krzysztof Czerwinski <substrifer@gmail.com>
2019-04-07 17:06:27 +0100

    Added resolution scaling

--- space_rocks/game.h
+++ space_rocks/game.h
@@ -3,5 +3,8 @@
 #include "scenes/scene_game.h"
 #include "scenes/scene_menu.h"
 
+#define GAMEX 1280
+#define GAMEY 720
+
 extern MenuScene menuScene;
 extern GameScene gameScene;
--- space_rocks/scenes/scene_menu.cpp
+++ space_rocks/scenes/scene_menu.cpp
@@ -7,13 +7,90 @@
 using namespace std;
 using namespace sf;
 
+std::shared_ptr<Entity> title;
+
+// Create FloatRect to fits Game into Screen while preserving aspect
+sf::FloatRect CalculateViewport(const sf::Vector2u& screensize,
+	const sf::Vector2u& gamesize) {
+
+	const Vector2f screensf(screensize.x, screensize.y);
+	const Vector2f gamesf(gamesize.x, gamesize.y);
+	const float gameAspect = gamesf.x / gamesf.y;
+	const float screenAspect = screensf.x / screensf.y;
+	float scaledWidth;  // final size.x of game viewport in piels
+	float scaledHeight; //final size.y of game viewport in piels
+	bool scaleSide = false; // false = scale to screen.x, true = screen.y
+
+	//Work out which way to scale
+	if (gamesize.x > gamesize.y) { // game is wider than tall
+	  // Can we use full width?
+		if (screensf.y < (screensf.x / gameAspect)) {
+			//no, not high enough to fit game height
+			scaleSide = true;
+		}
+		else {
+			//Yes, use all width available
+			scaleSide = false;
+		}
+	}
+	else { // Game is Square or Taller than Wide
+   // Can we use full height?
+		if (screensf.x < (screensf.y * gameAspect)) {
+			// No, screensize not wide enough to fit game width
+			scaleSide = false;
+		}
+		else {
+			// Yes, use all height available
+			scaleSide = true;
+		}
+	}
+
+	if (scaleSide) { // use max screen height
+		scaledHeight = screensf.y;
+		scaledWidth = floor(scaledHeight * gameAspect);
+	}
+	else { //use max screen width
+		scaledWidth = screensf.x;
+		scaledHeight = floor(scaledWidth / gameAspect);
+	}
+
+	//calculate as percent of screen
+	const float widthPercent = (scaledWidth / screensf.x);
+	const float heightPercent = (scaledHeight / screensf.y);
+
+	return sf::FloatRect(0, 0, widthPercent, heightPercent);
+}
+
+void UpdateScaling()
+{
+	const sf::Vector2u screensize(1280, 720);
+	const sf::Vector2u gamesize(GAMEX, GAMEY);
+	//set View as normal
+	Engine::GetWindow().setSize(screensize);
+	sf::FloatRect visibleArea(0.f, 0.f, gamesize.x, gamesize.y);
+	auto v = sf::View(visibleArea);
+	// figure out how to scale and maintain aspect;
+	auto viewport = CalculateViewport(screensize, gamesize);
+	//Optionally Center it
+	bool centered = true;
+	if (centered) {
+		viewport.left = (1.0 - viewport.width) * 0.5;
+		viewport.top = (1.0 - viewport.height) * 0.5;
+	}
+	//set!
+	v.setViewport(viewport);
+	Engine::GetWindow().setView(v);
+}
+
 void MenuScene::Load() {
   cout << "Menu Load \n";
   {
     auto txt = makeEntity();
     auto t = txt->addComponent<TextComponent>(
         "Platformer\nPress Space to Start");
+	txt->setPosition(Vector2f(500.0f, 32.0f));
   }
+  UpdateScaling();
   setLoaded(true);
 }
 
@@ -27,3 +104,4 @@ void MenuScene::Update(const double& dt) {
 
   Scene::Update(dt);
 }
+
