c201033b3795a6d9e050cc63c83cbbe58de3c042
Torantulino <40276179@live.napier.ac.uk>
2019-04-14 22:02:26 +0100

    Added fragment method.

--- space_rocks/components/cmp_destructible.cpp
+++ space_rocks/components/cmp_destructible.cpp
@@ -20,17 +20,26 @@ float DestructibleComponent::getHp() const { return _hp; }
 
 float DestructibleComponent::getMaxHp() const { return _maxHp; }
 
-void DestructibleComponent::damage(const float hp)
+void DestructibleComponent::damage(const float hp, const b2Vec2 coords)
 {
-	if (hp > 0.0f)
+	//Take damage
 		_hp -= hp;
+
+	//Still alive
+	//if (hp > 0.0f)
+	//{
+	//}
+	
+	//Destroied
 	if (_hp <= 0.0f)
 	{
-		//todo should spawn particles/other entities
 		_parent->setAlive(false);
 		_parent->setVisible(false);
 		_parent->setForDelete();
 	}
+
+	//Spawn impact fragments
+	spawnFragments(coords);
 }
 
 void DestructibleComponent::repair(const float hp)
@@ -39,4 +48,48 @@ void DestructibleComponent::repair(const float hp)
 		_hp += hp;
 	if (_hp > _maxHp)
 		_hp = _maxHp;
+}
+
+//Spawn the fragments relevent to the type of collision at the given coords
+void DestructibleComponent::spawnFragments(const b2Vec2 coords) 
+{
+	switch (_id)
+	{
+		//0: Bullet (Any)
+		case 0:
+			//TODO: Spawn bullet particles for all bullet impacts, regardless if they kill
+			break;
+		//1: Player
+		case 1:
+			//TODO: Player death explosion/particles
+			break;
+		//2: Enemy (Any)
+		case 2:
+			//TODO: Enemy death explosion/particles
+			break;
+		//3-10: Reserved for expansion
+		
+		//11+: Asteroids
+		//11: Asteroid 1, Size 1
+		case 11:
+			AsteroidFactory::makeAsteroid(121);
+			AsteroidFactory::makeAsteroid(122);
+			AsteroidFactory::makeAsteroid(123);
+			break;
+		//121: Asteroid 1, Size 2, No. 1
+		case 121:
+			AsteroidFactory::makeAsteroid(1311);
+			AsteroidFactory::makeAsteroid(1312);
+			break;
+		//122: Asteroid 1, Size 2, No. 2
+		case 122:
+			AsteroidFactory::makeAsteroid(1321);
+			AsteroidFactory::makeAsteroid(1322);
+			break;
+		//131: Asteroid 1, Size 2, No. 3
+		case 123:
+			AsteroidFactory::makeAsteroid(1331);
+			AsteroidFactory::makeAsteroid(1332);
+			break;
+	}
 }
--- space_rocks/components/cmp_destructible.h
+++ space_rocks/components/cmp_destructible.h
@@ -1,19 +1,21 @@
 #pragma once
 
 #include "ecm.h"
+#include "Box2D/Box2D.h"
 
 class DestructibleComponent : public Component {
 
 public:
 	DestructibleComponent() = delete;
-	explicit DestructibleComponent(Entity* p, const float hp);
+	explicit DestructibleComponent(Entity* p, const float hp, const unsigned int id);
 
 	void render() override { } 
 	void update(double dt) override;
 	float getHp() const;
 	float getMaxHp() const;
-	void damage(const float hp);
+	void damage(const float hp, const b2Vec2 coords);
 	void repair(const float hp);
+	void spawnFragments(const b2Vec2 coords);
 
 	~DestructibleComponent() override = default;
 
