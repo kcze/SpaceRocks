7a7d502f1ac1636b9f8cf97672b6fd32799c1de2
Torantulino <40276179@live.napier.ac.uk>
2019-04-15 01:56:30 +0100

    Overhaul of ship factory.

--- space_rocks/ship_factory.h
+++ space_rocks/ship_factory.h
@@ -1,13 +1,35 @@
 #pragma once
 
 #include "ecm.h"
+#include "engine.h"
+#include "game.h"
+#include "components\cmp_sprite.h"
+#include "components\cmp_physics.h"
+#include "components\cmp_ship.h"
+#include "components\cmp_destructible.h"
+#include "components\cmp_player.h"
+#include "system_resources.h"
+#include "Box2D/Box2D.h"
+#include <string>
+#include "system_physics.h"
+
+struct ObjectData
+{
+	std::vector<b2Vec2 > _coords;
+	std::shared_ptr<sf::Texture> _tex;
+	sf::IntRect _texRect;
+};
 
 class ShipFactory {
 public:
 	static std::shared_ptr<Entity> makePlayer();
-	static std::shared_ptr<Entity> makeEnemy();
+	static std::shared_ptr<Entity> makeEnemy(unsigned int type);
 	static std::shared_ptr<Entity> makeShip();
 
 private:
 	ShipFactory() {}
+	static std::map < unsigned int, ObjectData> _objectData;
+	static const float PSI8;
 };
+
+const float  ShipFactory::PSI8 = Physics::physics_scale_inv * 16.0f;
